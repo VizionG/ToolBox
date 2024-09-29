c040fd98cfd72e1b3e678ceeb6206e1fc0ae5066
Vizion <tito_goa@hotmail.com>
2024-09-29 23:06:58 +0100

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -3,12 +3,12 @@ Add-Type -AssemblyName PresentationFramework, PresentationCore, WindowsBase
 $toolboxPath = Join-Path -Path $env:TEMP -ChildPath "ToolBox"
 
 # Load other scripts
-. .SoftwareCategories.ps1
-. .Functions.ps1
-. .Styles.ps1
-. .Colors.ps1
-. .UI.ps1
-. .Settings.ps1
+. SoftwareCategories.ps1
+. Functions.ps1
+. Styles.ps1
+. Colors.ps1
+. UI.ps1
+. Settings.ps1
 
 # Create a DockPanel to use in the main window
 $dockPanel = New-Object -TypeName System.Windows.Controls.DockPanel
