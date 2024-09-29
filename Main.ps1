5828c6b00f00507fc55bf0c95c4616ff7a1d8135
Vizion <tito_goa@hotmail.com>
2024-09-29 23:59:17 +0100

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,11 +1,37 @@
 Add-Type -AssemblyName PresentationFramework, PresentationCore, WindowsBase
 
-. .\Scripts\SoftwareCategories.ps1
-. .\Scripts\Functions.ps1
-. .\Scripts\Styles.ps1
-. .\Scripts\Colors.ps1
-. .\Scripts\UI.ps1
-. .\Scripts\Settings.ps1
+# Set the path to the ToolBox directory in the TEMP folder
+$toolboxPath = Join-Path -Path $env:TEMP -ChildPath "ToolBox"
+
+# Construct full file paths from the toolboxPath
+$softwareCategoriesPath = Join-Path -Path $toolboxPath -ChildPath "SoftwareCategories.ps1"
+$functionsPath = Join-Path -Path $toolboxPath -ChildPath "Functions.ps1"
+$stylesPath = Join-Path -Path $toolboxPath -ChildPath "Styles.ps1"
+$colorsPath = Join-Path -Path $toolboxPath -ChildPath "Colors.ps1"
+$uiPath = Join-Path -Path $toolboxPath -ChildPath "UI.ps1"
+$settingsPath = Join-Path -Path $toolboxPath -ChildPath "Settings.ps1"
+
+# Load the scripts with error handling
+$scriptFiles = @(
+    $softwareCategoriesPath,
+    $functionsPath,
+    $stylesPath,
+    $colorsPath,
+    $uiPath,
+    $settingsPath
+)
+
+foreach ($script in $scriptFiles) {
+    if (Test-Path $script) {
+        try {
+            . $script
+        } catch {
+            Write-Host "Error loading script_"
+        }
+    } else {
+        Write-Host "File not found: $script"
+    }
+}
 
 # Create the main window
 $mainWindow = New-Object -TypeName System.Windows.Window
