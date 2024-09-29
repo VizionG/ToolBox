3bcc0830ea376e36d3c4cad2d89f047b77ca59e6
Vizion <tito_goa@hotmail.com>
2024-09-29 19:34:20 +0100

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,10 +1,18 @@
+# URL of the script hosted on GitHub (raw version)
+$SoftwareCategories = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/SoftwareCategories.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
+$Functions = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Functions.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
+$Styles = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Styles.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
+$Colors = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Colors.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
+$UI = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/UI.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
+$Settings = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Settings.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
+
 # Load other scripts
-. .\Scripts\SoftwareCategories.ps1
-. .\Scripts\Functions.ps1
-. .\Scripts\Styles.ps1
-. .\Scripts\Colors.ps1
-. .\Scripts\UI.ps1
-. .\Scripts\Settings.ps1
+. $SoftwareCategories
+. $Functions
+. $Styles
+. $Colors
+. $UI
+. $Settings
 
 # Create the main window
 $mainWindow = New-Object -TypeName System.Windows.Window
@@ -20,4 +28,3 @@ $mainWindow.Content = $dockPanel
 
 # Show the main window
 $mainWindow.ShowDialog()
-
