82ed37da60a00c30c7787c4a8e6ace833e998632
Vizion <tito_goa@hotmail.com>
2024-09-29 19:37:02 +0100

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,18 +1,26 @@
-# URL of the script hosted on GitHub (raw version)
-$SoftwareCategories = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/SoftwareCategories.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
-$Functions = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Functions.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
-$Styles = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Styles.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
-$Colors = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Colors.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
-$UI = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/UI.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
-$Settings = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Settings.ps1?token=GHSAT0AAAAAACYGXXYJ2F4N352AJ5R47GXSZXZTV2A"
+# URLs of the scripts hosted on GitHub (raw version)
+$SoftwareCategories = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/SoftwareCategories.ps1"
+$Functions = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Functions.ps1"
+$Styles = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Styles.ps1"
+$Colors = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Colors.ps1"
+$UI = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/UI.ps1"
+$Settings = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Settings.ps1"
 
-# Load other scripts
-. $SoftwareCategories
-. $Functions
-. $Styles
-. $Colors
-. $UI
-. $Settings
+# Function to load a script from a URL
+function Load-ScriptFromUrl {
+    param (
+        [string]$url
+    )
+    Invoke-RestMethod -Uri $url | Invoke-Expression
+}
+
+# Load other scripts from GitHub
+Load-ScriptFromUrl $SoftwareCategories
+Load-ScriptFromUrl $Functions
+Load-ScriptFromUrl $Styles
+Load-ScriptFromUrl $Colors
+Load-ScriptFromUrl $UI
+Load-ScriptFromUrl $Settings
 
 # Create the main window
 $mainWindow = New-Object -TypeName System.Windows.Window
@@ -23,7 +31,7 @@ $mainWindow.ResizeMode = 'CanResize'
 $mainWindow.WindowStartupLocation = 'CenterScreen'
 $mainWindow.Background = New-Object -TypeName System.Windows.Media.SolidColorBrush -ArgumentList ([System.Windows.Media.Color]::FromArgb(255, 38, 37, 38))
 
-# Assign dockPanel as the window's content
+# Assign dockPanel as the window's content (Make sure $dockPanel is defined in the loaded scripts)
 $mainWindow.Content = $dockPanel
 
 # Show the main window
