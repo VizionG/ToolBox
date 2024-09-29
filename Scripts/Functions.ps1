6488820e86e3b88a580028922a74e6f84add7f1c
Vizion <tito_goa@hotmail.com>
2024-09-29 19:47:53 +0100

    Test1

--- Scripts/Functions.ps1
+++ Scripts/Functions.ps1
@@ -1,30 +1,3 @@
-Add-Type -AssemblyName PresentationCore
-Add-Type -AssemblyName PresentationFramework
-Add-Type -AssemblyName WindowsBase
-
-$SoftwareCategories = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/SoftwareCategories.ps1"
-$Functions = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Functions.ps1"
-$Styles = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Styles.ps1"
-$Colors = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Colors.ps1"
-$UI = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/UI.ps1"
-$Settings = "https://raw.githubusercontent.com/VizionG/ToolBox/refs/heads/main/Scripts/Settings.ps1"
-
-# Function to load a script from a URL
-function Load-ScriptFromUrl {
-    param (
-        [string]$url
-    )
-    Invoke-RestMethod -Uri $url | Invoke-Expression
-}
-
-# Load other scripts from GitHub
-Load-ScriptFromUrl $SoftwareCategories
-Load-ScriptFromUrl $Functions
-Load-ScriptFromUrl $Styles
-Load-ScriptFromUrl $Colors
-Load-ScriptFromUrl $UI
-Load-ScriptFromUrl $Settings
-
 function Get-InstalledApps {
     $statusBox.Text = "Checking. . ."
     Update-UI
