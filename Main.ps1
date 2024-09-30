8c863fb42ec5fce4446daa5517d934d287e3069a
Vizion <tito_goa@hotmail.com>
2024-09-30 01:48:09 +0100

    Main

--- Main.ps1
+++ Main.ps1
@@ -41,6 +41,10 @@ $mainWindow.Height = 625
 $mainWindow.ResizeMode = 'CanResize'
 $mainWindow.WindowStartupLocation = 'CenterScreen'
 $mainWindow.Background = New-Object -TypeName System.Windows.Media.SolidColorBrush -ArgumentList ([System.Windows.Media.Color]::FromArgb(255, 38, 37, 38))
+
+# Assign dockPanel as the window's content
+$mainWindow.Content = $dockPanel
+
 $mainWindow.Add_Closing({
     # Cleanup: Remove the temporary directory after the window is closed
     Remove-Item -Path $mainScriptPath -Recurse -Force -ErrorAction SilentlyContinue
@@ -48,9 +52,6 @@ $mainWindow.Add_Closing({
     Write-Host "Cleaning up before closing..."
     # Optionally, you can add any code to release resources or save settings
 })
-
-# Assign dockPanel as the window's content
-$mainWindow.Content = $dockPanel
 
 # Show the main window
 $mainWindow.ShowDialog()
--- Start.ps1
+++ Start.ps1
@@ -2,40 +2,6 @@ Add-Type -AssemblyName PresentationFramework, PresentationCore, WindowsBase
 Get-ExecutionPolicy
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-# Function to download script from URL
-function Load-ScriptFromUrl {
-    param (
-        [string]$url,
-        [string]$subDir = "Scripts"  # Default subdirectory under ToolBox, unless specified
-    )
-    try {
-        Write-Host "Attempting to download script from: $url"
-        
-        # Determine the directory based on whether a subdirectory is provided
-        if ($subDir) {
-            $tempDir = Join-Path -Path $env:TEMP -ChildPath "ToolBox\$subDir"
-        } else {
-            $tempDir = Join-Path -Path $env:TEMP -ChildPath "ToolBox"
-        }
-
-        # Ensure the directory exists
-        $null = New-Item -ItemType Directory -Path $tempDir -ErrorAction SilentlyContinue
-
-        # Get the file name from the URL
-        $scriptName = [System.IO.Path]::GetFileName($url)
-        $tempScriptPath = Join-Path -Path $tempDir -ChildPath $scriptName
-
-        # Download the script from the URL
-        Invoke-WebRequest -Uri $url -OutFile $tempScriptPath -ErrorAction Stop
-        
-        Write-Host "Successfully downloaded script to: $tempScriptPath"
-        return $tempScriptPath
-    } catch {
-        Write-Error "Failed to load script from $url. Error: $_"
-        return $null
-    }
-}
-
 # Function to download and load all required scripts
 function DownloadAndLoadScripts {
     # Define script URLs
