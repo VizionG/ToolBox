60f70e8cb69de219ad4b7a062e04b5c02ec3590b
Vizion <tito_goa@hotmail.com>
2024-10-06 13:09:12 +0100

    Update Functions.ps1

--- Scripts/Functions.ps1
+++ Scripts/Functions.ps1
@@ -27,7 +27,7 @@ function Uninstall-Software {
     try {
         $statusBox.Text = "Uninstalling: $app_name"
         Update-UI
-        $command = "winget uninstall --id ${app_id} --silent"
+        $command = "winget uninstall --id ${app_id} --disable-interactivity --silent --accept-source-agreements --accept-package-agreements"
         Start-Process -NoNewWindow -FilePath "cmd.exe" -ArgumentList "/c $command" -Wait
         $statusBox.Text = "Successfully uninstalled: $app_name"
         Update-UI
