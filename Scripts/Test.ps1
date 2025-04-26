6bdb859febae41d245f4e5f66290a3e98a966350
Vizion <tito_goa@hotmail.com>
2025-04-26 15:12:44 +0100

    Sidebar

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -1,70 +1,44 @@
-# Create a DockPanel as the main layout container
-$dockPanel = New-Object -TypeName System.Windows.Controls.DockPanel
-$dockPanel.HorizontalAlignment = 'Stretch'
-$dockPanel.VerticalAlignment = 'Stretch'
-$dockPanel.LastChildFill = $true
+# --- Sidebar + Content in a New Tab ---
 
-# Create a Grid for the main layout (just one column for the sidebar)
-$mainGrid = New-Object -TypeName System.Windows.Controls.Grid
-$mainGrid.HorizontalAlignment = 'Stretch'
-$mainGrid.VerticalAlignment = 'Stretch'
+# Assume $tabControl, $tabItemStyle, $buttonStyle, $whitebrush, $brushbackground, etc. are already available
 
-# Single column (sidebar only)
-$sidebarColumn = New-Object System.Windows.Controls.ColumnDefinition
-$sidebarColumn.Width = '180'
-$mainGrid.ColumnDefinitions.Add($sidebarColumn)
+# Create a new TabItem for the sidebar/content demo
+$sidebarTab = New-Object System.Windows.Controls.TabItem
+$sidebarTab.Header = "Sidebar Demo"
+$sidebarTab.Style = $tabItemStyle
 
-# Create a Grid for the sidebar section (on the right)
-$sidebarGrid = New-Object -TypeName System.Windows.Controls.Grid
-$sidebarGrid.HorizontalAlignment = 'Stretch'
-$sidebarGrid.VerticalAlignment = 'Stretch'
+# Create a new grid for the tab layout
+$sidebarTabGrid = New-Object System.Windows.Controls.Grid
+$sidebarTabGrid.HorizontalAlignment = 'Stretch'
+$sidebarTabGrid.VerticalAlignment = 'Stretch'
+$sidebarTabGrid.ColumnDefinitions.Add((New-Object System.Windows.Controls.ColumnDefinition -Property @{Width='*'}))    # Content column
+$sidebarTabGrid.ColumnDefinitions.Add((New-Object System.Windows.Controls.ColumnDefinition -Property @{Width='180'}))  # Sidebar column
 
-# Define rows for the sidebar (status and logos)
-$sidebarGrid.RowDefinitions.Add((New-Object System.Windows.Controls.RowDefinition))  # Logos/status row
+# --- Content for the left side ---
+$leftContent = New-Object System.Windows.Controls.TextBlock
+$leftContent.Text = "This is the content area (left side)."
+$leftContent.Margin = 20
+$leftContent.VerticalAlignment = 'Center'
+$leftContent.HorizontalAlignment = 'Center'
+$sidebarTabGrid.Children.Add($leftContent)
+[System.Windows.Controls.Grid]::SetColumn($leftContent, 0)
 
-# Add Vizion logo
-$vizionLogo = New-Object -TypeName System.Windows.Controls.Image
-$vizionLogo.HorizontalAlignment = 'Center'
-$vizionLogo.VerticalAlignment = 'Top'
-$vizionLogo.Margin = '5, 5, 5, 5'
-$imageSource = "https://viziong.github.io/ToolBox/Resources/images/v_logo.png"
-$vizionLogo.Source = [System.Windows.Media.Imaging.BitmapImage]::new([System.Uri]::new($imageSource))
-$sidebarGrid.Children.Add($vizionLogo)
+# --- Sidebar for the right side ---
+$sidebarDemoGrid = New-Object System.Windows.Controls.StackPanel
+$sidebarDemoGrid.Background = [System.Windows.Media.Brushes]::LightGray
+$sidebarDemoGrid.Margin = 10
 
-# Add Windows logo
-$windowsLogo = New-Object -TypeName System.Windows.Controls.Image
-$windowsLogo.HorizontalAlignment = 'Center'
-$windowsLogo.VerticalAlignment = 'Top'
-$windowsLogo.Margin = '5, 150, 5, 5'
-$windowsVersion = Get-WindowsVersion
-if ($windowsVersion -like "*11*") {
-    $imageSource = "https://viziong.github.io/ToolBox/Resources/images/Windows_11_logo.png"
-} elseif ($windowsVersion -like "*10*") {
-    $imageSource = "https://viziong.github.io/ToolBox/Resources/images/Windows_10_logo.png"
-}
-$windowsLogo.Source = [System.Windows.Media.Imaging.BitmapImage]::new([System.Uri]::new($imageSource))
-$sidebarGrid.Children.Add($windowsLogo)
+$sidebarLabel = New-Object System.Windows.Controls.TextBlock
+$sidebarLabel.Text = "Sidebar (right side)"
+$sidebarLabel.FontWeight = 'Bold'
+$sidebarLabel.Margin = 5
+$sidebarDemoGrid.Children.Add($sidebarLabel)
 
-# Add the status TextBox
-$statusBox = New-Object -TypeName System.Windows.Controls.TextBox
-$statusBox.Height = 60
-$statusBox.Width = 170
-$statusBox.Margin = '5, 190, 5, 5'
-$statusBox.IsReadOnly = $true
-$statusBox.Text = "Detect: $windowsVersion"
-$statusBox.FontWeight = '600'
-$statusBox.TextAlignment = 'Center'
-$statusBox.Foreground = $whitebrush
-$statusBox.Background = $brushbackground
-$statusBox.Padding = '10,10,10,10'
-$statusBox.BorderThickness = '2'
-$statusBox.TextWrapping = 'Wrap'
-$statusBox.AcceptsReturn = $true
-$sidebarGrid.Children.Add($statusBox)
+$sidebarTabGrid.Children.Add($sidebarDemoGrid)
+[System.Windows.Controls.Grid]::SetColumn($sidebarDemoGrid, 1)
 
-# Add sidebarGrid to mainGrid
-$mainGrid.Children.Add($sidebarGrid)
-[System.Windows.Controls.Grid]::SetColumn($sidebarGrid, 0)
+# Set the grid as the content of the new tab
+$sidebarTab.Content = $sidebarTabGrid
 
-# Add mainGrid to dockPanel
-$dockPanel.Children.Add($mainGrid)
+# Add the new tab to the TabControl
+$tabControl.Items.Add($sidebarTab)
